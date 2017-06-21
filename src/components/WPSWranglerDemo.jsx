2d7c275df801cf67cf480f231973b202006b7239
maartenplieger <maarten.plieger@knmi.nl>
2017-06-21 14:14:49 +0200

    Refactored wrangler demo

--- src/components/WPSWranglerDemo.jsx
+++ src/components/WPSWranglerDemo.jsx
@@ -3,15 +3,6 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Button } from 'reactstrap';
 
-// let wpsProcessOptions = [];
-
-// let dataInputs = 'inputCSVPath=ExportOngevalsData100lines.csv;metaCSVPath=metaDataCsv.json;jobDescPath=jobDesc.json;limit=101';
-// wpsProcessOptions['wrangleProcess'] = 'service=wps&request=Execute&identifier=wrangleProcess&' +
-//      'version=1.0.0&DataInputs=' + dataInputs + '&storeExecuteResponse=true&status=true&';
-
-// wpsProcessOptions['binaryoperatorfornumbers_10sec'] = 'service=wps&request=Execute&identifier=binaryoperatorfornumbers_10sec&version=1.0.0&' +
-//   'DataInputs=inputa=10;inputb=2;operator=divide;&storeExecuteResponse=true&status=true&';
-
 export default class WPSWranglerDemo extends Component {
   constructor () {
     super();
@@ -20,17 +11,24 @@ export default class WPSWranglerDemo extends Component {
 
   wrangleClicked (id) {
     const { accessToken, dispatch, actions, nrOfStartedProcesses } = this.props;
+    dispatch(actions.startWPSExecute(accessToken, 'wrangleProcess',
+      'inputCSVPath=ExportOngevalsData100lines.csv;metaCSVPath=metaDataCsv.json;jobDescPath=jobDesc.json;limit=101',
+      nrOfStartedProcesses));
+  };
+
+  calculateClicked (id) {
+    const { accessToken, dispatch, actions, nrOfStartedProcesses } = this.props;
     dispatch(actions.startWPSExecute(accessToken, 'binaryoperatorfornumbers_10sec', 'inputa=10;inputb=2;operator=divide', nrOfStartedProcesses));
   };
 
   render () {
-    const { accessToken, nrOfStartedProcesses, runningProcesses, nrOfRunningProcesses, nrOfFailedProcesses, nrOfCompletedProcesses } = this.props;
+    const { accessToken, nrOfStartedProcesses, runningProcesses, nrOfFailedProcesses, nrOfCompletedProcesses } = this.props;
     return (
       <div className='MainViewport'>
         <h1>Wrangler Demo</h1>
         <p>{accessToken}</p>
         <Button id='wrangleButton' onClick={() => { this.wrangleClicked('wrangleProcess'); }}>Wrangle!</Button>
-        <Button id='wrangleButton' onClick={() => { this.wrangleClicked('binaryoperatorfornumbers_10sec'); }}>Calculator</Button>
+        <Button id='wrangleButton' onClick={() => { this.calculateClicked('binaryoperatorfornumbers_10sec'); }}>Calculator</Button>
         <p>nrOfStartedProcesses: {nrOfStartedProcesses}</p>
         <p>nrOfFailedProcesses: {nrOfFailedProcesses}</p>
         <p>nrOfCompletedProcesses: {nrOfCompletedProcesses}</p>
