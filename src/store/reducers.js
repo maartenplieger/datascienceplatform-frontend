e1f7d31126f33144ce68ffc1817560416ca5f21e
maartenplieger <maarten.plieger@knmi.nl>
2017-06-21 09:42:43 +0200

    Fixed linting errors

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -2,20 +2,17 @@ import { combineReducers } from 'redux';
 import locationReducer from '../reducers/location';
 import wrangleReducer from '../reducers/wrangleReducer';
 import userReducer from '../reducers/userReducer';
-import uploadReducer from '../reducers/uploadReducer';
 import { combineForms } from 'react-redux-form';
-import { initialFileDescription } from '../constants/initialFormStates';
 
 import basketReducer from '../reducers/basketReducer';
 export const makeRootReducer = () => {
-
   return combineReducers({
     location: locationReducer,
     wranglerState: wrangleReducer,
     userState: userReducer,
     basketState: basketReducer,
     fileDescriptionState: combineForms({
-      fileDescription: ""
+      fileDescription: ''
     })
   });
 };
