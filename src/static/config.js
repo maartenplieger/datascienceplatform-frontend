5961f438d756943d3abb962f14cc52a1a8cecd7d
Rob <private@tjalma.com>
2017-06-22 13:12:24 +0000

    removed config file from repo

--- src/static/config.js
+++
@@ -1,5 +0,0 @@
-export const config = {
-  // 'backendHost': 'https://pc150232.knmi.nl:9000',
-  'backendHost': 'https://pc150232.knmi.nl:9001',
-  'adagucServicesHost': 'https://bhw451.knmi.nl:8090'
-};
