19f8b531d4064f72c240bfcd5cc60b0b21ade516
wagenaar <saskia.wagenaar@knmi.nl>
2017-06-22 07:21:39 +0000

    Cleaned up the code, added proptypes.

--- src/components/FileStructureDescriptionComponent.jsx
+++ src/components/FileStructureDescriptionComponent.jsx
@@ -1,115 +1,113 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { config } from 'static/config.js';
-import { connect } from 'react-redux';
 import { Control, Form } from 'react-redux-form';
-import { actions } from 'react-redux-form';
-import { initialFileStructureDescription } from '../constants/initialFormStates'
 
-export default class FileColumnDescriptionComponent extends React.Component {
-
-  constructor () {
-    super();
-  }
-
-  handleSubmit() {
-
-    const { backendHost, frontendHost, adagucServicesHost } = config;
+export default class FileStructureDescriptionComponent extends Component {
+  handleSubmit () {
     const { dispatch, actions } = this.props;
 
-    var fileName = this.props.fileName.replace(/\.[^/.]+$/, "_descr.json");
+    var fileName = this.props.fileName.replace(/\.[^/.]+$/, '_descr.json');
 
-    var formData  = new FormData();
-    formData.append("files",new Blob([JSON.stringify( this.props.fileStructureDescription,this.props.replacer)],{type:""}), fileName);
+    var formData = new FormData();
+    formData.append('files', new Blob([JSON.stringify( this.props.fileStructureDescription,this.props.replacer)], { type:'' }), fileName);
 
-    fetch(adagucServicesHost + "/basket/upload?key=" + this.props.accessToken,
+    fetch(config.adagucServicesHost + '/basket/upload?key=' + this.props.accessToken,
       {
-        credentials:"include",
-        method: "POST",
+        credentials:'include',
+        method: 'POST',
         body: formData
       })
       .then(function(result) {
+        // TODO: Foutafhandeling
         console.log(result);
         dispatch(actions.setUploadedFileStructureDescription());
-      })
-
+      });
   }
 
-  render() {
-
-    console.log(this.props.fileStructureDescription.firstDataRow);
-
+  render () {
     return (
       <div>
-      <div className="alert alert-info">
-        Please specify the structure of the CSV file.
-      </div>
-      <div>
-      <Form model="fileStructureDescription"
+        <div className='alert alert-info'>
+          Please specify the structure of the CSV file.
+        </div>
+        <div>
+          <Form model='fileStructureDescription'
             onSubmit={() => this.handleSubmit()}>
 
-        <h5 className="media-heading">File structure information:</h5>
+            <h5 className='media-heading'>File structure information:</h5>
 
-        <div className="form-group row">
-          <label className="col-3 col-form-label required">Column separator:</label>
-          <Control.select
-            className="form-control col-3"
-            model="fileStructureDescription.columnSeparator"
-            required
-          >
-            <option value=""></option>
-            <option value=",">comma (,)</option>
-            <option value=";">semicolon (;)</option>
-            <option value="tab">tab (\t)</option>
-          </Control.select>
-        </div>
+            <div className='form-group row'>
+              <label className='col-3 col-form-label required'>Column separator:</label>
+              <Control.select
+                className='form-control col-3'
+                model='fileStructureDescription.columnSeparator'
+                required
+              >
+                <option value='' />
+                <option value=','>comma (,)</option>
+                <option value=';'>semicolon (;)</option>
+                <option value='tab'>tab (\t)</option>
+              </Control.select>
+            </div>
 
-        <div className="form-group row">
-          <label className="col-3 col-form-label required">Row separator:</label>
-          <Control.select
-            className="form-control col-3"
-            model="fileStructureDescription.rowSeparator"
-            required
-          >
-            <option value=""></option>
-            <option value="CR">Carriage return (\r)</option>
-            <option value="LF">Line feed (\n)</option>
-            <option value="CRLF">Carriage return line feed (\r\n)</option>
-          </Control.select>
-        </div>
+            <div className='form-group row'>
+              <label className='col-3 col-form-label required'>Row separator:</label>
+              <Control.select
+                className='form-control col-3'
+                model='fileStructureDescription.rowSeparator'
+                required
+              >
+                <option value='' />
+                <option value='CR'>Carriage return (\r)</option>
+                <option value='LF'>Line feed (\n)</option>
+                <option value='CRLF'>Carriage return line feed (\r\n)</option>
+              </Control.select>
+            </div>
 
-        <div className="form-group row">
-          <label className="col-3 col-form-label required">First data row:</label>
-          <Control
-            className="form-control col-3 required"
-            type="number"
-            model="fileStructureDescription.firstDataRow"
-            required
-            min={0}
-            value={this.props.fileStructureDescription.firstDataRow}
-          />
-        </div>
+            <div className='form-group row'>
+              <label className='col-3 col-form-label required'>First data row:</label>
+              <Control
+                className='form-control col-3 required'
+                type='number'
+                model='fileStructureDescription.firstDataRow'
+                required
+                min={0}
+                value={this.props.fileStructureDescription.firstDataRow}
+              />
+            </div>
 
-        <div className="form-group row">
-          <label className="col-3 col-form-label">Column name row:</label>
-          <Control
-            className="form-control col-3"
-            type="number"
-            model="fileStructureDescription.rowWithFieldNames"
-            min={0}
-          />
-          <small className="text-muted form-text col-6">(optional if you want to see the column names in the preview)</small>
-        </div>
+            <div className='form-group row'>
+              <label className='col-3 col-form-label'>Column name row:</label>
+              <Control
+                className='form-control col-3'
+                type='number'
+                model='fileStructureDescription.rowWithFieldNames'
+                min={0}
+              />
+              <small className='text-muted form-text col-6'>(optional if you want to see the column names in the
+                preview)
+              </small>
+            </div>
 
-        <div className="divider-2" />
+            <div className='divider-2' />
 
-        <button type="submit" className="btn btn-primary">
-          Submit description
-        </button>
-      </Form>
+            <button type='submit' className='btn btn-primary'>
+              Submit description
+            </button>
+          </Form>
         </div>
-        </div>
+      </div>
     );
   }
-
 }
+
+FileStructureDescriptionComponent.propTypes = {
+  fileColumnDescription: PropTypes.object,
+  fileName: PropTypes.string,
+  accessToken: PropTypes.string,
+  dispatch: PropTypes.func.isRequired,
+  actions: PropTypes.object.isRequired,
+  fileStructureDescription: PropTypes.object,
+  replacer: PropTypes.func
+};
--- src/components/UploadComponent.jsx
+++ src/components/UploadComponent.jsx
@@ -1,68 +1,90 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { config } from 'static/config.js';
-import { connect } from 'react-redux';
-import { Control, Form } from 'react-redux-form';
-import { actions } from 'react-redux-form';
-import FileColumnDescriptionComponent from './FileColumnDescriptionComponent'
-import FileStructureDescriptionComponent from './FileStructureDescriptionComponent'
-import FileUploadComponent from './FileUploadComponent'
-import ScrollArea from 'react-scrollbar';
+import FileColumnDescriptionComponent from './FileColumnDescriptionComponent';
+import FileStructureDescriptionComponent from './FileStructureDescriptionComponent';
+import PreviewComponent from './PreviewComponent';
+import FileUploadComponent from './FileUploadComponent';
 
-export default class UploadComponent extends React.Component {
-
-  componentWillUnmount() {
+export default class UploadComponent extends Component {
+  componentWillUnmount () {
     console.log(this.props.actions);
     this.props.dispatch(this.props.actions.clearUploadState());
   }
 
-  replacer(name, val) {
+  replacer (name, val) {
+
+    console.log("REPLACE!");
+
     // Convert integers to integers.
     if (parseInt(val)) {
       return parseInt(val);
     }
-    if (val == "tab") {
-      return "\t";
+    if (val === 'tab') {
+      return '\t';
     }
-    if (val == "CR") {
-      return "\r";
+    if (val === 'CR') {
+      return '\r';
     }
-    if (val == "LF") {
-      return "\n";
+    if (val === 'LF') {
+      return '\n';
     }
-    if (val == "CRLF") {
-      return "\r\n";
+    if (val === 'CRLF') {
+      return '\r\n';
     }
 
     // Return the value as is.
     return val;
   };
 
-  render() {
+  render () {
+
+    // TODO: Replace is niet correct doorgestuurd!!!!!
+
     return (
       <div>
         <FileUploadComponent accessToken={this.props.accessToken}
                              dispatch={this.props.dispatch} actions={this.props.actions} />
-        <div className="divider-2" />
+        <div className='divider-2' />
         { this.props.fileName &&
         <FileStructureDescriptionComponent accessToken={this.props.accessToken}
-                                           dispatch={this.props.dispatch}
-                                           actions={this.props.actions}
-                                           fileName={this.props.fileName}
-                                           fileStructureDescription={this.props.fileStructureDescription}
-                                           replace={this.replace}/>
+          dispatch={this.props.dispatch}
+          actions={this.props.actions}
+          fileName={this.props.fileName}
+          fileStructureDescription={this.props.fileStructureDescription}
+          replace={this.replace}/>
         }
-        <div className="divider-2" />
+
+        <div className='divider-2' />
+        {this.props.fileName && this.props.uploadedFileStructureDescription &&
+        <PreviewComponent
+          file={config.adagucServicesHost + '/opendap/' + this.props.accessToken + '/' + this.props.clientId.replace('/','.') +'/' + this.props.fileName}
+          tableClassName='previewTable'
+          componentClassName='previewComponent'
+          numberOfLinesDisplayed={30}/>
+        }
+
+        <div className='divider-2' />
         { this.props.fileName && this.props.uploadedFileStructureDescription &&
         <FileColumnDescriptionComponent accessToken={this.props.accessToken}
-                                        dispatch={this.props.dispatch}
-                                        actions={this.props.actions}
-                                        fileName={this.props.fileName}
-                                        fileColumnDescription={this.props.fileColumnDescription}
-                                        fileStructureDescription={this.props.fileStructureDescription}
-                                        replacer={this.replacer}/>
+          dispatch={this.props.dispatch}
+          actions={this.props.actions}
+          fileName={this.props.fileName}
+          fileColumnDescription={this.props.fileColumnDescription}
+          fileStructureDescription={this.props.fileStructureDescription}
+          replacer={this.replacer} />
         }
       </div>
     );
   }
 }
+
+UploadComponent.propTypes = {
+  fileName: PropTypes.string,
+  uploadedFileStructureDescription: PropTypes.bool,
+  accessToken: PropTypes.string,
+  fileColumnDescription: PropTypes.object,
+  fileStructureDescription: PropTypes.object,
+  dispatch: PropTypes.func.isRequired,
+  actions: PropTypes.object.isRequired
+};
